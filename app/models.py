16b9e3484ac393ab41aed6a80bb0802ef508d9a4
lolek <lesyaolexs852@gmail.com>
2022-07-01 22:17:07 +0300

    Add model for users

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
+import uuid
 
+from django.db import models
+
+
+class CustomUser(models.Model):
+    SEX_CHOICES = (
+        (
+            "male",
+            "Male",
+        ),
+        (
+            "female",
+            "Female",
+        ),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    login = models.CharField(unique=True, max_length=16)
+    sex = models.CharField(choices=SEX_CHOICES, max_length=6)
+    birth_date = models.DateTimeField()
